72a14e804fdbbd235198d7fc97a64ed13c95341a
eirikst <eirikstadheim@EMPR.local>
2016-01-19 19:04:30 +0100

    Initial commit, example project

---
+++ spring-rest/src/main/java/hello/GreetingController.java
@@ -0,0 +1,38 @@
+package hello;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(1L, "tester543");
+    }
+
+    @RequestMapping("/test")
+    public Greeting greeting() {
+        SessionFactory sessionFactory = new Configuration().configure()
+                .buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+
+        AppUser user = new AppUser("firstuser");
+        session.save(user);
+
+        session.getTransaction().commit();
+        session.close();
+
+        return new Greeting(1L, "tester12345");
+    }
+}
