4d7ba81d29a8ba5b068c98bb675a5d799e34a858
eirikstadheim <eirik_stadheim@outlook.com>
2016-04-01 12:42:05 +0200

    Difficulty included in event model

--- src/main/java/com/andreasogeirik/tools/Constants.java
+++ src/main/java/com/andreasogeirik/tools/Constants.java
@@ -7,5 +7,9 @@ public class Constants {
     public static final int NUMBER_OF_POSTS_RETURNED = 5;
     public static final int NUMBER_OF_EVENTS_RETURNED = 5;
     public static final int NUMBER_OF_USERS_RETURNED_SEARCH = 5;
-    public static final String BACKEND_URL = "http://sportydul.azurewebsites.net/";
+    public static final int EVENT_DIFFICULTY_EASY = 1;
+    public static final int EVENT_DIFFICULTY_MEDIUM = 2;
+    public static final int EVENT_DIFFICULTY_HARD = 3;
+    //public static final String BACKEND_URL = "http://sportydul.azurewebsites.net/";
+    public static final String BACKEND_URL = "http://78.91.3.136:8080/";
 }
