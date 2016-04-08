6444e9d5a46775fd06318d30c2a1ce8e2ab901c6
eirikstadheim <eirik_stadheim@outlook.com>
2016-04-08 16:32:23 +0200

    Transaction management 2k

--- src/main/java/com/andreasogeirik/security/WebSecurityConfig.java
+++ src/main/java/com/andreasogeirik/security/WebSecurityConfig.java
@@ -7,15 +7,16 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled=true)
 @EnableScheduling
+@EnableTransactionManagement
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
